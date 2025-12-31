d6edbb4134e1bd76b2ea641db5708216f3d72cdb
Veltris Admin <veltris.compliance@gmail.com>
2025-12-31 20:00:09 +0100

    Minor Updates

--- core/settings.py
+++ core/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage'
+    'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'django.contrib.humanize',
